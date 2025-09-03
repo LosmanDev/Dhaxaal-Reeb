c0ee054de7bce00d39d969526fce60aae6e3d39f
liban <losmandev@gmail.com>
2025-09-02 21:02:12 -0400

    first

---
+++ app/components/navbar.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <Image
+            src="/dr-bg.png"
+            width={50}
+            height={50}
+            alt="Dhaxaal-reeb logo"
+          ></Image>
+          <span className="ml-3 text-xl">Dhaxal-Reeb</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900">First Link</a>
+          <a className="mr-5 hover:text-gray-900">Second Link</a>
+          <a className="mr-5 hover:text-gray-900">Third Link</a>
+          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
